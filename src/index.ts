64dbd69b9ede84e5404bff5103895ed987bfecb6
José Bezerra <jose.bezerra@tempest.com.br>
2022-04-06 14:57:29 -0300

    ready

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import ffmpeg from 'fluent-ffmpeg'
+import fs from 'fs'
+import express, { Express, Request, Response } from 'express';
+const getStat = require('util').promisify(fs.stat);
+
+const app: Express = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+const library = ['1f9a2a33-ee65-431b-9495-d254b3b7bbce', '163aa61e-7802-4973-986e-842267acdc97', 'ff00a1e2-29a2-4560-b094-379408c40c6f'];
+
+app.get('/generate', async (req: Request, res: Response) => {
+    for(let i=0; i<10; i++){
+        const fileName = library[2];
+        const filePath = `./public/${fileName}.mp3`;
+        const randomStartTime = Math.floor(Math.random() * 120) + 1;
+        ffmpeg(filePath).setStartTime(randomStartTime).setDuration(15).format('mp3')
+        .saveToFile(`./tmp/${fileName}-${randomStartTime}.mp3`).on('end', (output) => {
+            console.log('done!');
+        })
+    }
+});
+
+app.get('/:fileName', async (req: Request, res: Response) => {
+    const TMP_PATH = './tmp';
+    const fileName = req.params.fileName;
+    const files: string[] = [];
+    fs.readdirSync(TMP_PATH).forEach(file => {
+        if(file.startsWith(fileName)){
+            files.push(file);
+        }
+    });
+
+    const randomFilesIndex = Math.floor(Math.random() * files.length) + 0;
+    const choosenFileName = files[randomFilesIndex];
+    const filePath = `${TMP_PATH}/${choosenFileName}`;
+    const stat = await getStat(filePath);
+    res.writeHead(200, {
+        'Content-Type': 'audio/mp3',
+        'Content-Length': stat.size
+    });
+
+    const stream = fs.createReadStream(filePath);
+    stream.pipe(res);
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
